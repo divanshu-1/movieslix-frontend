92bff643d35d84c5c738a96e1cb641f67b40238e
Divanshu Arora <139143122+divanshu-1@users.noreply.github.com>
2025-04-12 17:38:40 +0530

    Add files via upload

---
+++ MovieslixFrontEnd/README.md
@@ -0,0 +1,17 @@
+# MoviesLix
+
+Hi! This is Fullstack Video Rental Website with NoSQL Database
+
+# Tech-Stack
+
+MongoDb, Express.js, React.js, Node.js
+
+## How To Run
+
+Install the node modules with
+
+    npm install
+
+Then run the app with 
+
+    npm start
---
+++ MovieslixFrontEnd/src/components/common/input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Input = ({ name, label, error, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input {...rest} name={name} id={name} autoComplete="off" className="form-control" />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
